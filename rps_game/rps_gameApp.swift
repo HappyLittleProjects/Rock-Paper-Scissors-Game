e67ca709bdcfbf3dc63184da11f111e4312d1d8f
HappyLittleProjects <lindalau81@gmail.com>
2022-10-11 15:35:33 -0700

    Initial Commit

---
+++ rps_game/rps_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  rps_gameApp.swift
+//  rps_game
+//
+//  Created by Linda Lau on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct rps_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
